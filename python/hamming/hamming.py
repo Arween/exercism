b7c0f454de648058b12116e69eff0d149905027f
Arween <semka.alesya@gmail.com>
2017-05-07 12:45:56 +0300

    hamming

---
+++ python/hamming/hamming.py
@@ -0,0 +1,16 @@
+class Hamming(object):
+
+    def __init__(self, list_dna, list_mistake_dna):
+        self.list_dna = list_dna
+        self.list_mistake_dna = list_mistake_dna
+
+    def distance(self):
+        count = 0
+        for dna in self.list_dna:
+            for mistake_dna in self.list_mistake_dna:
+                if dna != mistake_dna:
+                    count += 1
+        return count
+
+hamming = Hamming()
+hamming.distance()
